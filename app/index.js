433ceb8d58a2229d083a197a9d2f76378fcb77ea
Barry Weinberg <bjw2119@gmail.com>
2016-09-21 15:16:19 -0400

    Sort array of top 5

---
+++ app/index.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+module.exports = function(){
+    let root = path.join(__dirname, '../');
+    let npmPath = path.join(root, './node_modules');
+    let browserPath = path.join(root, './browser');
+    let homePath = path.join(root, './app/views/index.html');
+    console.log(homePath);
+
+    app.use(express.static(npmPath));
+    app.use(express.static(browserPath));
+    app.use(express.static(homePath));
+
+    app.get('/*', function(req, res){
+        console.log("Finding home");
+        res.sendFile(homePath);
+    });
+
+    app.use(function(err, req, res, next){
+        console.error(err);
+        res.status(err.status).send(err.message);
+    });
+
+    return app;
+}
